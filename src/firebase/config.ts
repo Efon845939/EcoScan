32ca1408be960e98dc293ffe7e24d25bcf6be94c
Gamer Efe Türk Küçükvardar <proturkgamerefe@gmail.com>
2025-12-11 08:35:51 +0000

    Try fixing this error: `Console Error: [2025-12-11T08:32:05.646Z] @fireb

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
   "projectId": "studio-8170073529-138bb",
   "appId": "1:1042954996945:web:deb96330bbcb653155c9f0",
+  "storageBucket": "studio-8170073529-138bb.appspot.com",
   "apiKey": "AIzaSyAA-aU0vquPpPVbbFg5Bzh2vNsAQHFHG5o",
   "authDomain": "studio-8170073529-138bb.firebaseapp.com",
-  "measurementId": "",
   "messagingSenderId": "1042954996945"
 };
